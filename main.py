f7ec4879d6ee88a487aacd0a9afdfb7a5e971116
dzmitryi-lazarchyk <dzmitryi.lazarchyk1994@gmail.com>
2023-02-18 17:32:16 +0300

    Create columns, image and description

---
+++ main.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.png", width=450)
+
+with col2:
+    st.title("Dzmitryi Lazarchyk")
+    content = """
+    Hello! I am Dzmitryi! I am Phython developer. Check out my study projects,
+    that I provided below."""
+    st.info(content)
